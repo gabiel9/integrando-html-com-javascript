4860273ea6d2a136911dff3549e7c8372eac7131
Gabriel dos Santos <byeldossantos@gmail.com>
2023-10-30 21:20:47 -0300

    att

--- main.js
+++ main.js
@@ -2,22 +2,30 @@ const form = document.getElementById('form-deposito');
 
 function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
-    return nomeComoArray.lenght >= 2;
+    return nomeComoArray.length >= 2;
 }
 
 form.addEventListener('submit', function(e) {
-    let FormEValido = false;
+    let formEValido = false;
     e.preventDefault();
 
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: ${valorDeposito} depositando para o cliente: ${nomeBeneficiario} - conta: ${numeroContaBeneficiario}`;
+    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositando para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;
 
-    if (!validaNome(nomeBeneficiario.value )) {
+    
+    formEValido = validaNome(nomeBeneficiario.value)
+    if (formEValido) {
         alert(mensagemSucesso);
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+
+
     } else {
-        alert("Tudo certo");
+        alert("O nome está incomlpeto");
     }
 })
 
